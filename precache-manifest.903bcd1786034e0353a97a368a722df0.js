fb3857ff68f23f532ea06ea7e8a3a480049c31b9
LuisSullca <luis.sullca.h@uni.pe>
2020-07-20 21:00:42 -0500

    Updates

---
+++ precache-manifest.903bcd1786034e0353a97a368a722df0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f88ad2d276098b300aceceb4a5d123a9",
+    "url": "/adminRed/index.html"
+  },
+  {
+    "revision": "404efde2a25140d9f7d2",
+    "url": "/adminRed/static/css/main.6a480d3c.chunk.css"
+  },
+  {
+    "revision": "18bc8344015ed6e92b1f",
+    "url": "/adminRed/static/js/2.61689bdb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/adminRed/static/js/2.61689bdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "404efde2a25140d9f7d2",
+    "url": "/adminRed/static/js/main.305412fc.chunk.js"
+  },
+  {
+    "revision": "0d4d56617b08c809f794",
+    "url": "/adminRed/static/js/runtime-main.0c6459ad.js"
+  }
+]);
